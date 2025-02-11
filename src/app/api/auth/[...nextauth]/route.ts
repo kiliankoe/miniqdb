8074436010d9ee96cbad45aa0dd215b22f86a3c7
Kilian Koeltzsch <me@kilian.io>
2025-02-11 23:59:51 +0100

    add nextauth route

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import { AuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    verifyRequest: "/auth/verify-request", // Used for check email message
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
