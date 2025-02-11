335767e39c0cef4b29d694690a639fcbdbae04b8
Kilian Koeltzsch <me@kilian.io>
2025-02-12 00:01:38 +0100

    validate vote value

--- src/app/api/quotes/[quoteId]/vote/route.ts
+++ src/app/api/quotes/[quoteId]/vote/route.ts
@@ -1,12 +1,16 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function PUT(request: NextRequest) {
   const params = await request.nextUrl.searchParams;
   const quoteId = params.get("quoteId");
   const authorId = params.get("authorId");
   const vote = params.get("vote");
+  if (vote !== "1" && vote !== "-1") {
+    return NextResponse.json({ error: "Invalid vote" }, { status: 400 });
+  }
   console.log(quoteId, authorId, vote);
   // const quote = await getQuote(quoteId);
   // const updatedQuote = await updateQuote(quoteId, { vote });
   // return NextResponse.json(updatedQuote);
+  return NextResponse.json({ quoteId, authorId, vote });
 }
