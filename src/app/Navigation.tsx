2d70434087a68d35e3d5a216b6439ff524dce6ea
Kilian Koeltzsch <me@kilian.io>
2025-02-12 16:51:29 +0100

    only full-reload random on repeated clicks

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -7,7 +7,11 @@ export default function Navigation() {
     <nav className="mb-6 space-x-2 text-sm font-semibold underline">
       <Link href="/">new</Link>
       <Link href="/?sort=top">top</Link>
-      <Link href="/?sort=random" onClick={() => window.location.reload()}>random</Link>
+      <Link href="/?sort=random" onClick={() => {
+        if (window.location.search === "?sort=random") {
+          window.location.reload();
+        }
+      }}>random</Link>
       <Link href="/submit">submit</Link>
     </nav>
   )
