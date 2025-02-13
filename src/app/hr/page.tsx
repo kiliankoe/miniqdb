15afc632c3676bacf48c0408237eca87c61f25c9
Kilian Koeltzsch <me@kilian.io>
2025-02-13 23:38:34 +0100

    show close hint on hr page

--- src/app/hr/page.tsx
+++ src/app/hr/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { Container, Typography } from "@mui/material";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function HRPage() {
+  const [isShowingCloseHint, setIsShowingCloseHint] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowingCloseHint(true);
+    }, 10_000);
+  }, []);
+
   return (
     <Container
       maxWidth="md"
@@ -16,6 +27,11 @@ export default function HRPage() {
     >
       <Typography variant="h4">Please enjoy this GIF of a cute goat.</Typography>
       <Image src="/goat.gif" alt="Goat" width={490} height={498} />
+      {isShowingCloseHint && (
+        <Typography variant="h6">
+          You may now close this tab.
+        </Typography>
+      )}
     </Container>
   );
 }
