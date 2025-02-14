653db72eb39decf91818c8354c68770c54363f26
Kilian Koeltzsch <me@kilian.io>
2025-02-14 23:54:43 +0100

    formatting

--- src/app/api/quotes/[quoteId]/route.ts
+++ src/app/api/quotes/[quoteId]/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getQuote } from "../GetQuotes";
 import { authOptions } from "../../auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
+
 export async function GET(request: NextRequest, { params }: { params: Promise<{ quoteId: string }> }) {
   const session = await getServerSession(authOptions);
   const { quoteId } = await params;
