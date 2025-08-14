9e79ec17b9727acbc78e9cb6269f8bcc42f75693
Kilian Koeltzsch <me@kilian.io>
2025-08-14 18:17:45 +0200

    fix hydration errors

--- src/app/(main)/ClientLayout.tsx
+++ src/app/(main)/ClientLayout.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { createTheme, CssBaseline } from "@mui/material";
+import { createTheme, CssBaseline, useMediaQuery } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import Header from "./Header";
 
 const queryClient = new QueryClient();
@@ -14,31 +14,19 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [mode, setMode] = useState<"light" | "dark">("light");
-
-  useEffect(() => {
-    const prefersDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
-    setMode(prefersDarkMode ? "dark" : "light");
-
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const handleChange = (e: MediaQueryListEvent) => {
-      setMode(e.matches ? "dark" : "light");
-    };
-
-    mediaQuery.addEventListener("change", handleChange);
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)", {
+    noSsr: true,
+    defaultMatches: false,
+  });
 
   const theme = useMemo(
     () =>
       createTheme({
         palette: {
-          mode,
+          mode: prefersDarkMode ? "dark" : "light",
         },
       }),
-    [mode],
+    [prefersDarkMode],
   );
 
   const appName = process.env.NEXT_PUBLIC_MINIQDB_NAME || "miniqdb";
